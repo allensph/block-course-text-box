bb34e6525ab2ed727c028e9bf91d7ffd102ab759
allensph <allensph@gmail.com>
2022-03-18 16:18:05 +0800

    change icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd39adc3c67b75c5d8cf3e4418fff44b3');
